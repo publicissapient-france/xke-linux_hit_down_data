cda4550e2cad4be07e8c0a97545a9c8fca545613
ndechandon <ndechandon@xebia.fr>
2018-02-12 08:57:11 +0100

    fix story thong

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ docker rmi linuxhitdowndata/first
 echo "build..."
 docker build -t 'linuxhitdowndata/first' -f docker/Dockerfile .  1>/dev/null
 
+BUILD_RETURN_CODE=$?
+
 echo "Now execute ./run.sh"
+
